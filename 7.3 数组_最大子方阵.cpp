d5fb6e910b813e3f64b5536ab98a87dce6de151a
liangsonghua <1411008034@qq.com>
2016-06-03 20:21:48 +0800

    Changes to be committed:

--- 7.3 数组_最大子方阵.cpp
+++ 7.3 数组_最大子方阵.cpp
@@ -1,66 +1,70 @@
 class SubMatrix {
 public:
     int maxSubMatrix(vector<vector<int> > mat, int n) {
-        //任何一个点的下方(包括自己)1的个数存于数组bottom[i][j]
-        //任何一个点的右边(包括自己)1的个数存于数组right[i][j]
+      
+        if(n == 0) return 0;
+        vector<vector<int> > matA = mat;//坐标点下方连续1的个数
+        vector<vector<int> > matB = mat;//坐标点右方连续1的个数
+        vector<vector<int> > matAA = mat;//坐标点下方连续0的个数
+        vector<vector<int> > matBB = mat;//坐标点右方连续0的个数
         //数组分别往上和往左填充
-        //检验四条边颜色是否相同为时,往右或往下跳k个判断1个数是否为k或为0个
-        vector<vector<int> > bottom = mat ,right = mat;
-        for(int i=n-1;i>=0;i--)
-        {
-           if(mat[n-1][i]==1)
-           {
-               bottom[n-1][i] =  1;
-           }
-           if(mat[i][n-1]==1)
-           {
-               right[i][n-1] = 1;
-           }
-        }
-         
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j=n-2;j>=0;j--)
-            {
-               if(mat[i][j]==1)
-               {
-                   bottom[i][j]= bottom[i+1][j]+1;
-                   right[i][j]= right[i][j+1]+1;
-               }
-               else
-               {
-                   bottom[i][j]= bottom[i+1][j];
-                   right[i][j]= right[i][j+1];
-               }
+        //检验时,往右或往下跳边长个判断1或者0的个数是否为边长长度
+        int i,j;
+        int len = 0;
+        int wide;
+        for(i=n-1;i>=0;--i){
+            for(j=n-1;j>=0;--j){
+                if(mat[i][j] == 0){
+                    matA[i][j] = 0;
+                    matB[i][j] = 0;
+                    if(i == n-1){
+                        matAA[i][j] = 1;
+                    }else{
+                        matAA[i][j] = matAA[i+1][j]+1;
+                    }
+                    if(j == n-1){
+                        matBB[i][j] = 1;
+                    }else{
+                        matBB[i][j] = matBB[i][j+1]+1;
+                    }
+                }else {
+                    if(i == n-1){
+                        matA[i][j] = 1;
+                    }else{
+                        matA[i][j] = matA[i+1][j]+1;
+                    }
+                    if(j == n-1){
+                        matB[i][j] = 1;
+                    }else{
+                        matB[i][j] = matB[i][j+1]+1;
+                    }
+                    matAA[i][j] = 0;
+                    matBB[i][j] = 0;
+                }
             }
         }
-         
-        int len = 0;
-        int squreLen = 0;
-        for(int i=0;i<n;i++)
-        {
-          for(int j=0;j<n;j++)
-          {
-              for(int k=j;k<n;k++)
-              {
-                  if(mat[i][j]==0)
-                  {
-                      squreLen = 0;
-                  }
-                  else
-                  {
-                      squreLen = k-j;
-                  }
-                   
-                  if( bottom[i][j]>=squreLen && right[i][j]>=squreLen
-                     &&  (bottom[i][j]-bottom[i][k]) ==squreLen
-                     && (right[i][j]-right[k][j]) == squreLen )
-                    {
-                        len = max(len,k-j+1);
-                    } 
-              }
-           }
+       for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                if(mat[i][j] == 0){
+                    wide = min(matAA[i][j],matBB[i][j]);
+                    while(wide>0){
+                        if(matAA[i][j+wide-1] >= wide && matBB[i+wide-1][j] >= wide){
+                            len = len<wide?wide:len;
+                        }
+                        wide--;
+                    }
+                }else{
+                    wide = min(matA[i][j],matB[i][j]);
+                    while(wide>0){
+                        if(matA[i][j+wide-1] >= wide && matB[i+wide-1][j] >= wide){
+                            len = len<wide?wide:len;
+                        }
+                        wide--;
+                    }
+                }
+            }
         }
         return len;
     }
 };
+添加笔记
